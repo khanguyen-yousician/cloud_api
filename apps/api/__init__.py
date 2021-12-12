6bf3666e0ede0bdbc8bfb6a5603877d7fe47cf41
Kha Nguyen <>
2021-12-12 20:58:18 +0200

    Fix blueprint issue

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -4,6 +4,7 @@ from packages.exceptions.exceptions import AivenApiException
 
 api = Blueprint('api', __name__)
 
+from apps.api import cloud
 
 @api.errorhandler(AivenApiException)
 def aiven_exception_handlers(e):
